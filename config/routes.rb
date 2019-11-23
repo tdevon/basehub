115f41a087ff6469a66714e12cdef96d1dfd0668
Tyler <tylerdevon01@gmail.com>
2019-11-22 22:21:14 -0500

    Added dashboard controller + projects scaffold + config custom devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,19 @@
 Rails.application.routes.draw do
-  devise_for :users
   root to:'home#index'
+  devise_for :users, controllers: { sessions: 'users/sessions' }
+
+  devise_scope :user do
+    get 'login', to: 'devise/sessions#new'
+    get 'register', to: 'devise/registrations#new'
+  end
+
+  # Dashboard
+  get 'dashboard', to: "dashboard#index"
+
+  # Projects
+  #get 'projects', to: "projects#index"
+  resources :projects
+
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_22_041833) do
+ActiveRecord::Schema.define(version: 2019_11_23_021849) do
+
+  create_table "projects", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_projects_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -24,4 +33,5 @@ ActiveRecord::Schema.define(version: 2019_11_22_041833) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "projects", "users"
 end
